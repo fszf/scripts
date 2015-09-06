6dd137f7d8ec17cfb860901354ea5e47bd957a83
frank604 <frankshin82@gmail.com>
2015-09-05 20:59:04 -0700

    clean it up

---
+++ autostart.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+# autostart apps
+
+xrdb -merge ~/.Xresources 
+
--- vimprobtab.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Script to open Vimprobable instances in tabbed
-
-exec vimprobable2 -e $(</tmp/tabbed.xid) "$1" &
-
