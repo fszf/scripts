66fc8b9dd117b1b4b7f45fc5dc948507488deb2c
frank604 <frankshin82@gmail.com>
2015-09-15 03:29:14 -0700

    add brightness script

---
+++ bright.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+# display brightness script
+# author: lswest
+
+usage="usage: $0 -c {up|down}"
+command=
+increment=5%
+
+while getopts h o
+do case "$o" in
+    h) echo "$usage"; exit 0;;
+    ?) echo "$usage"; exit 0;;
+esac
+done
+
+shift $(($OPTIND - 1))
+command=$1
+
+if [ "$command" = "" ]; then
+    echo "usage: $0 {up|down}"
+    exit 0;
+fi
+
+display_brightness=0
+
+if [ "$command" = "up" ]; then
+    $(xbacklight -inc $increment)
+    display_brightness=$(xbacklight|cut --delimiter="." -f 1)
+fi
+
+if [ "$command" = "down" ]; then
+    $(xbacklight -dec $increment)
+    display_brightness=$(xbacklight|cut --delimiter="." -f 1)
+fi
+
+icon_name=""
+
+if [ "$icon_name" = "" ]; then
+    if [ "$display_brightness" = "0" ]; then
+        icon_name="notification-display-brightness-off"
+    else
+        if [ "$display_brightness" -lt "33" ]; then
+            icon_name="notification-display-brightness-low"
+        else
+            if [ "$display_brightness" -lt "67" ]; then
+                icon_name="notification-display-brightness-medium"
+            else
+                if [ "$display_brightness" -lt "100" ]; then
+                     icon_name="notification-display-brightness-high"
+                else
+                     icon_name="notification-display-brightness-full"
+                fi
+            fi
+        fi
+    fi
+fi
+notify-send " " -i $icon_name -h int:value:$display_brightness -h string:synchronous:brightness
