6224e6959c7de58e4e876db929e0cc90ef6f9950
frank604 <frankshin82@gmail.com>
2015-09-20 23:56:55 -0700

    add diablo run script

---
+++ diablo.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ~/.wine/drive_c/Program\ Files\ \(x86\)/Diablo\ III/
+setarch i386 -L -B -R -3 /usr/bin/wine "/home/frank604/.wine/drive_c/Program Files (x86)/Diablo III/Diablo III Launcher.exe" -launch -opengl
+
